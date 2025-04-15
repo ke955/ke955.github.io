357aa26aa313a66f710ee00389b966fc1ec16f02
ke955 <sujita@ruri.waseda.jp>
2025-04-15 14:49:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeMessage() {
   const messages = [
-    "フォートナイト最高",
-    "プログラミング面白い",
-    "プログラミング頑張ります",
-    "よろしくお願いします"
+    "フォートナイト最高 🎮",
+    "プログラミング面白い 💻",
+    "プログラミング頑張ります 💪",
+    "よろしくお願いします！🙌"
   ];
   const randomIndex = Math.floor(Math.random() * messages.length);
   document.getElementById("message").textContent = messages[randomIndex];
